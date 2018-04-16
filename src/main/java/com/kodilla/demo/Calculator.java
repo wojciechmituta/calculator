9ee6f9a18405bc01c16b1736e52999025902e8b1
“wojciechmituta <38225947+wojciechmituta@users.noreply.github.com>
2018-04-16 18:27:55 +0200

    first commit

---
+++ src/main/java/com/kodilla/demo/Calculator.java
@@ -0,0 +1,12 @@
+package com.kodilla.demo;
+
+public class Calculator {
+    public void SomeMethod() {
+        System.out.println("Its Work");
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.SomeMethod();
+    }
+}
